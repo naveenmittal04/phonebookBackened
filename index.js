a3521517b6b24cafce8200d75694042b468808c4
Naveen Mittal <naveenmittal04@gmail.com>
2023-07-08 18:59:05 +0530

    part 3 phonebook backend step 8 done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
-morgan.token('type', function (req, ) { return JSON.stringify(req.body) })
+morgan.token('type', function (req) { return JSON.stringify(req.body) })
 
 app.use(express.json())
 app.use(morgan(':method :url :status :req[content-length] - :response-time ms :type\n'))
