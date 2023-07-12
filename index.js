16bc996cdebdf33a426276e8ebeec934dce74588
Naveen Mittal <naveenmittal04@gmail.com>
2023-07-12 18:23:31 +0530

    Phonebook database, step3-4 done

--- index.js
+++ index.js
@@ -14,42 +14,49 @@ app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :req[content-length] - :response-time ms :type\n'))
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
     Person.find({})
         .then(persons => {
             response.json(persons)
         })
-        .catch(()=>{
+        .catch(error=>{
             console.log('Failed to get persons from DB')
+            next(error)
         })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id)
         .then(person => {
-            response.json(person)
+            if(person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
         })
-        .catch(()=>{
-            response.status(404).end()
+        .catch((error)=>{
+            next(error)
         })
 })
 
-app.get('/info', (request, response) => {
+app.get('/info', (request, response, next) => {
     Person.count({})
         .then(count => {
             response.end(`<p>Phonebook has info for ${count} people</p>`
                 +`<p> ${new Date()}</p>`)
         })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.deleteOne({_id:request.params.id})
         .then(()=>{
             response.status(204).end()
         })
+        .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const person = request.body
     console.log(`add /persons/ ${person} request received`)
     if(!person.hasOwnProperty('name')){
@@ -83,10 +90,34 @@ app.post('/api/persons', (request, response) => {
         .then(result => {
             response.json(result)
         })
-        .catch(()=>{
+        .catch(error=>{
             console.log('Failed to save to DB')
+            next(error)
         })
 })
+
+const unknowEndpoint = (request, response) => {
+    response.status(404).send({error: 'unknown endpoint'})
+}
+
+app.use(unknowEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+    if(error.name === 'CastError'){
+        return response.status(400).end({
+            error: 'malformed request'
+        })
+    } else if(error.name === 'BSONError') {
+        return response.status(400).end({
+            error: 'malformed request'
+        })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, ()=>{
